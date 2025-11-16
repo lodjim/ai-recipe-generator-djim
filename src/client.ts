7765e90e3a4dd55acf2879f079a4b54c846749d0
Abdoulaye FAYE <a-faye@hotmail.fr>
2025-11-16 19:28:38 +0000

    update UI

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
+import type { Schema } from "../amplify/data/resource";
+
 import { generateClient } from "aws-amplify/api";
-import { type Schema } from "../amplify/data/resource";
 import { createAIHooks } from "@aws-amplify/ui-react-ai";
 
 export const client = generateClient<Schema>({ authMode: "userPool" });
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from '../amplify_outputs.json';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Authenticator } from "@aws-amplify/ui-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+Amplify.configure(outputs);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+);
