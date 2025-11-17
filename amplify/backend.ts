3f646b9dc71094fd57b61a55009d91ad441a0ed3
Abdoulaye FAYE <a-faye@hotmail.fr>
2025-11-17 00:47:22 +0000

    access denied

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -25,9 +25,9 @@ const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
 bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
   new PolicyStatement({
     resources: [
-      `arn:aws:bedrock::us-east-1::foundation-model/anthropic.claude-3-5-sonnet-20241022-v2:0`,
+      `arn:aws:bedrock::us-east-1::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1:0`,
     ],
-    actions: ["bedrock:InvokeModel"],
+    actions: ["bedrock:InvokeModel", "bedrock:InvokeModelWithResponseStream"],
     
   })
 );
