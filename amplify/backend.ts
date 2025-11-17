f17528e9444c0be7ccaac38681b1e37f0e70756f
Abdoulaye FAYE <a-faye@hotmail.fr>
2025-11-17 13:56:55 +0000

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -25,7 +25,7 @@ const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
 bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
   new PolicyStatement({
     resources: [
-      `arn:aws:bedrock::us-east-1::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1:0`,
+      `arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1:0`,
     ],
     actions: ["bedrock:InvokeModel", "bedrock:InvokeModelWithResponseStream"],
     
